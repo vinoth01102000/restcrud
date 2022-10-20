271594b578654d6317263d86e73722fff7418eec
vinoth0110 <113687176+vinoth0110@users.noreply.github.com>
2022-10-20 14:49:02 +0530

    ini

---
+++ product/rest/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Book(models.Model):
+    title=models.CharField(max_length=100,blank=True)
+    author=models.CharField(max_length=100,blank=True)
+    publisher=models.CharField(max_length=100,blank=True)
+
+
+    def __str__(self):
+        return self.title
